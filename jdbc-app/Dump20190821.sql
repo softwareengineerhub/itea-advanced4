048ace7f4f6104e39ec0c2ebb0fa39694a651615
softwareengineerhub <denis.prokopiuk@yahoo.com>
2019-08-21 20:44:57 +0300

    Backup

--- jdbc-app/Dump20190821.sql
+++ jdbc-app/Dump20190821.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `itea_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `itea_db`;
 -- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
 --
 -- Host: localhost    Database: itea_db
@@ -54,4 +56,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-08-21 20:39:41
+-- Dump completed on 2019-08-21 20:44:16
